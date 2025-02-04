ddddf5b6b1c4d278503059fc8c3873956194d1b9
Samuel Zubak <samko.zubak2008@gmail.com>
2025-02-04 09:37:58 +0100

    uloha 5.1

--- uloha 5.py
+++ uloha 5.py
@@ -2,3 +2,4 @@ cenatovarusDPH=float(input("Zadaj cenu s DPH: "))
 DPH=float(input("Zadaj vysku DPH v percentach: "))
 cenabezDPH=cenatovarusDPH/(DPH/100+100)*100 #treba doriesit
 print ("cena bez DPH je: ", cenabezDPH)
+input ("Stlač ENTER pre ukončenie programu.")
