67a7716e9b10d7a1df12e4a07174889ff5d48c0f
Samuel Zubak <samko.zubak2008@gmail.com>
2025-02-11 08:33:55 +0100

    dalsie2 ulohy

--- drot.py
+++ drot.py
@@ -1,2 +1,5 @@
-dd=int(input("zadaj dlzku drotu: "))
-djk=int(input("zadaj dlzku 1 ks: "))
+dd=int(input("zadaj dlzku drotu v m: "))
+djk=int(input("zadaj dlzku 1 ks v m: "))
+pk=dd//djk
+zvysok=dd%djk
+print ("pocet kusov: ", pk, "zvys sa z drotu v m: ",zvysok,)
